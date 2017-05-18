5a60115b4ee3b6173973a20750f17e449e8c0e0a
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-18 12:07:46 -0400

    JSON working, gotta parse and populate HTML accordingly

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ign?callback=?', function(json){
+		var myStr = JSON.stringify(json);
+		$('#streamer-box-2').html(myStr);
+		var myObj = JSON.parse(myStr);
+		console.log(myObj);
+	});
+
 
 
 });
+
+var channels = ["ESL_SC2", 
+"OgamingSC2", 
+"cretetion", 
+"freecodecamp", 
+"storbeck", 
+"habathcx", 
+"RobotCaleb",
+"KindaFunnyGames",
+"ign"
+]
