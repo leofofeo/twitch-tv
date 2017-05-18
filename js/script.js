7abed7cd3501b9c31925b9feaf5970d28968b5af
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-18 15:00:23 -0400

    added tabs and classes for hiding/displaying data

--- js/script.js
+++ js/script.js
@@ -22,32 +22,54 @@ var callTwitchAPI = function(channel, counter){
 	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+ channel+'?callback=?', function(json){
 		var myStr = JSON.stringify(json);
 		var myObj = JSON.parse(myStr);
-		if(myObj.stream === null){
-			return;
-		};
-		parseStreamContent(myObj, counter);
+		// if(myObj.stream === null){
+		// 	return;
+		// };
+		parseStreamContent(channel, myObj, counter);
 		console.log(myObj);
 	});
 }
 
-var parseStreamContent = function(jsonObj, counter){
-	var imgSrc = jsonObj.stream.channel['logo'];
-	var channel = jsonObj.stream.channel['display_name'];
-	var url = jsonObj.stream.channel['url'];
-	var status = jsonObj.stream.channel['status'];
-	var game = jsonObj.stream.channel['game'];
+var parseStreamContent = function(channel, jsonObj, counter){
+	if(jsonObj.stream === null){
+		var imgSrc = 'http://res.cloudinary.com/leofofeo/image/upload/v1495132889/default-twitch_p3fwgm.jpg';
+		var channel = channel;
+		var url = '';
+		var content = "Offline";
+		var game = '';
+		var status = 'offline';
+	} else {
+		var imgSrc = jsonObj.stream.channel['logo'];
+		var channel = jsonObj.stream.channel['display_name'];
+		var url = jsonObj.stream.channel['url'];
+		var content = jsonObj.stream.channel['status'];
+		var game = jsonObj.stream.channel['game'];
+		var status = "live";		
+	};
 
-	displayStreamContent(imgSrc, channel, url, status, game, counter);
+	
+
+	displayStreamContent(imgSrc, channel, url, content, game, counter, status);
 }
 
 
-var displayStreamContent = function(imgSrc, channel, url, status, game, counter){
+var displayStreamContent = function(imgSrc, channel, url, content, game, counter, status){
 	var imgId = 'streamer-img-' + counter;
 	var channelId = 'streamer-channel-' + counter;
-	var streamerStatus = 'streamer-status-' + counter;
-	$('#streams-container').append('<div class="streamer-box"><span class="streamer-img" id="' + imgId + '"></span><span class="streamer-channel" id="' + channelId + '"></span><span class="streamer-status" id="' + streamerStatus + '"></span></div>');
+	var streamingContent = 'streamer-content-' + counter;
+	if (status === "live"){
+		$('#streams-container').append('<div class="row streamer-row streamer-box live"><div class="col-md-2"><span class="streamer-img" id="' + imgId + '"></span></div><div class="col-md-2"><span class="streamer-channel" id="' + channelId + '"></span></div><div class="col-md-8"><span class="streamer-content" id="' + streamingContent + '"></span></div></div>');	
+	} else {
+		$('#streams-container').append('<div class="row streamer-row streamer-box offline"><div class="col-md-2"><span class="streamer-img" id="' + imgId + '"></span></div><div class="col-md-2"><span class="streamer-channel" id="' + channelId + '"></span></div><div class="col-md-8"><span class="streamer-content" id="' + streamingContent + '"></span></div></div>');	
+	}
+	
 
 	$('#' + imgId).html('<img class="streamer-img img-responsive" src="' + imgSrc + '">')
 	$('#' + channelId).html(channel);
-	$('#' + streamerStatus).html(status);
+	if(content === 'Offline'){
+		$('#' + streamingContent).html(content);	
+	} else {
+		$('#' + streamingContent).html('Streaming: <em>' + content + '</em>');
+	}
+	
 }
