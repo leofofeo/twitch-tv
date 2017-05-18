50c49d5df8cf215672ab6f332e2b1bf66cdeb68b
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-18 13:28:55 -0400

    get call working for hardcoded channel, need to expand it to multiple channels, might need to use a different endpoint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ign?callback=?', function(json){
-		var myStr = JSON.stringify(json);
-		$('#streamer-box-2').html(myStr);
-		var myObj = JSON.parse(myStr);
-		console.log(myObj);
-	});
+	callTwitchAPI();
 
 
 
@@ -21,3 +16,30 @@ var channels = ["ESL_SC2",
 "KindaFunnyGames",
 "ign"
 ]
+
+var parseStreamContent = function(jsonObj){
+	var imgSrc = jsonObj.stream.channel['logo'];
+	var channel = jsonObj.stream.channel['display_name'];
+	var url = jsonObj.stream.channel['url'];
+	var status = jsonObj.stream.channel['status'];
+	var game = jsonObj.stream.channel['game'];
+
+	displayStreamContent(imgSrc, channel, url, status, game);
+}
+
+var callTwitchAPI = function(){
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ign?callback=?', function(json){
+		var myStr = JSON.stringify(json);
+		var myObj = JSON.parse(myStr);
+		parseStreamContent(myObj);
+		console.log(myObj);
+	});
+}
+
+var displayStreamContent = function(imgSrc, channel, url, status, game){
+	$('#streams-container').html('<div class="streamer-box"><span class="streamer-img" id="streamer-img-1"></span><span class="streamer-user" id="streamer-user-1"></span><span class="streamer-status" id="streamer-status-1"></span></div>');
+
+	$('#streamer-img-1').html('<img class="streamer-img img-responsive" src="' + imgSrc + '">')
+	$('#streamer-user-1').html(channel);
+	$('#streamer-status-1').html(status);
+}
