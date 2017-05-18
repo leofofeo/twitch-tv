4be304ae9d165525dc2dc6aeaa5225b3d0edef09
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-18 13:42:15 -0400

    got live streams displaying correctly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 //JS and jQuery for RQ
 $('document').ready(function(){
-	callTwitchAPI();
-
-
-
+	
+	for(var i = 0; i < channels.length; i++){
+		callTwitchAPI(channels[i], i);	
+	}
 });
 
 var channels = ["ESL_SC2", 
@@ -17,29 +17,37 @@ var channels = ["ESL_SC2",
 "ign"
 ]
 
-var parseStreamContent = function(jsonObj){
+
+var callTwitchAPI = function(channel, counter){
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+ channel+'?callback=?', function(json){
+		var myStr = JSON.stringify(json);
+		var myObj = JSON.parse(myStr);
+		if(myObj.stream === null){
+			return;
+		};
+		parseStreamContent(myObj, counter);
+		console.log(myObj);
+	});
+}
+
+var parseStreamContent = function(jsonObj, counter){
 	var imgSrc = jsonObj.stream.channel['logo'];
 	var channel = jsonObj.stream.channel['display_name'];
 	var url = jsonObj.stream.channel['url'];
 	var status = jsonObj.stream.channel['status'];
 	var game = jsonObj.stream.channel['game'];
 
-	displayStreamContent(imgSrc, channel, url, status, game);
+	displayStreamContent(imgSrc, channel, url, status, game, counter);
 }
 
-var callTwitchAPI = function(){
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ign?callback=?', function(json){
-		var myStr = JSON.stringify(json);
-		var myObj = JSON.parse(myStr);
-		parseStreamContent(myObj);
-		console.log(myObj);
-	});
-}
 
-var displayStreamContent = function(imgSrc, channel, url, status, game){
-	$('#streams-container').html('<div class="streamer-box"><span class="streamer-img" id="streamer-img-1"></span><span class="streamer-user" id="streamer-user-1"></span><span class="streamer-status" id="streamer-status-1"></span></div>');
+var displayStreamContent = function(imgSrc, channel, url, status, game, counter){
+	var imgId = 'streamer-img-' + counter;
+	var channelId = 'streamer-channel-' + counter;
+	var streamerStatus = 'streamer-status-' + counter;
+	$('#streams-container').append('<div class="streamer-box"><span class="streamer-img" id="' + imgId + '"></span><span class="streamer-channel" id="' + channelId + '"></span><span class="streamer-status" id="' + streamerStatus + '"></span></div>');
 
-	$('#streamer-img-1').html('<img class="streamer-img img-responsive" src="' + imgSrc + '">')
-	$('#streamer-user-1').html(channel);
-	$('#streamer-status-1').html(status);
+	$('#' + imgId).html('<img class="streamer-img img-responsive" src="' + imgSrc + '">')
+	$('#' + channelId).html(channel);
+	$('#' + streamerStatus).html(status);
 }
